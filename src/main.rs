987c45072b265e116cdb2a6e0933ae187395cc43
medfahmy <medfahmy42@gmail.com>
2025-03-28 16:31:44 +0000

    init tempo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{
-    fs::read_dir,
-    path::{Path, PathBuf},
+    path::PathBuf,
     process,
 };
 
@@ -15,32 +14,9 @@ fn main() -> Result<(), ()> {
         })?);
 
     if path.is_file() {
-        eprintln!("path='{}' is not a directory", path.display());
+        eprintln!("'{}' is not a directory", path.display());
         process::exit(1);
     }
 
-    let dirs = walk_dir(&path)?;
-    dbg!(dirs);
-
     Ok(())
-}
-
-fn walk_dir(path: &Path) -> Result<Vec<PathBuf>, ()> {
-    let entries: Vec<_> = read_dir(path)
-        .map_err(|err| {
-            eprintln!("error reading directory contents; error={}", err);
-            process::exit(1);
-        })?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, std::io::Error>>()
-        .map_err(|err| {
-            eprintln!("error reading directory children; error={}", err);
-            process::exit(1);
-        })?;
-
-    let dirs = entries.into_iter().filter(|path| path.is_dir()).collect();
-
-    // TODO: walk children directories
-
-    Ok(dirs)
 }
