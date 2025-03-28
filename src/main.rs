0dc1950f081b157b9088bfe17ed74c89f3ae870c
medfahmy <medfahmy42@gmail.com>
2025-03-28 18:14:05 +0000

    create sound file

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
-use std::{
-    path::PathBuf,
-    process,
-};
+use std::{fs::File, io::Write, process::Command};
+use zerocopy::IntoBytes;
 
-fn main() -> Result<(), ()> {
-    let args: Vec<_> = std::env::args().skip(1).collect();
+fn main() {
+    let buf: Vec<u8> = wave()
+        .iter()
+        .flat_map(|sample| sample.as_bytes())
+        .map(|b| *b)
+        .collect();
 
-    let path = args.first()
-        .map(|path| PathBuf::from(path))
-        .unwrap_or(std::env::current_dir().map_err(|err| {
-            eprintln!("error accessing current working directory; error={}", err);
-            process::exit(1);
-        })?);
+    // TODO: proper error handling
 
-    if path.is_file() {
-        eprintln!("'{}' is not a directory", path.display());
-        process::exit(1);
-    }
+    let mut file = File::create("output.bin").unwrap();
+    file.write_all(&buf).unwrap();
 
-    Ok(())
+    println!("wrote sound to output.bin");
+    println!("playing sound");
+
+    // ffplay -f f32le -ar 48000 output.bin
+    Command::new("ffplay")
+        .arg("-f")
+        .arg("f32le")
+        .arg("-ar")
+        .arg("48000")
+        .arg("output.bin")
+        .output()
+        .unwrap();
+}
+
+fn wave() -> Vec<f32> {
+    let max = 48000;
+    let seconds = 1;
+    let samples = 0..max * seconds;
+    let step = 0.05;
+    let volume = 0.5;
+
+    samples
+        .map(|sample| (sample as f32 * step * volume).sin())
+        .collect()
 }
